31342f136eb00b833693334c42ef22b99879d085
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-09-25 22:33:12 +0530

    fixed UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ export default function Home() {
       <SearchBar onSearch={handleSearch} />
 
       {user?.role === "recruiter" && (
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 py-8 border border-red-500">
+        <div className="max-w-4xl mx-auto px-4 sm:px-6 py-8 ">
           <h2 className="text-2xl font-bold text-gray-800 mb-4">
             Post a New Interview
           </h2>
