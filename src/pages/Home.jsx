e1134075fa274c1453e1f7f5b8db0973e870f60f
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-10-15 19:30:55 +0530

    add loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,30 @@ import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer";
 import BeginJourney from "../components/BeginJourney";
 import ContactPage from "./ContactPage";
-import Loader, { PageLoader } from '../components/Loader';
-import { useState } from "react";
+import Loader, { PageLoader } from "../components/Loader";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // if (isLoading) {
-  //   return <PageLoader />;
-  // }
+  useEffect(() => {
+    const alreadyLoaded = sessionStorage.getItem("homeLoaded");
+    if (alreadyLoaded) {
+      setIsLoading(false);
+      return;
+    }
 
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      sessionStorage.setItem("homeLoaded", "true");
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
   return (
     <div className="w-full ">
       <HeroSection />
