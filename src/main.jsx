7555b7d37fc88fd33d6deaba25eefe253a3d94b8
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-11-19 00:16:41 +0530

    auto scroll to top on page change

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ToastProvider } from "./components/Toast.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Provider store={store}>
         <ToastProvider>
           <App />
