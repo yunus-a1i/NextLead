4576578432a81d4af248eb65d2f92bcef7137499
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-12-20 16:44:08 +0530

    update backend api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://next-backend-2-yn5j.onrender.com/api", // adjust if needed
-  baseURL: "http://localhost:4000/api", // adjust if needed
+  baseURL: "https://next-backend-2-yn5j.onrender.com/api", // adjust if needed
+  // baseURL: "http://localhost:4000/api", // adjust if needed
   withCredentials: true, // if you're using cookies/session
   headers: {
     "Content-Type": "application/json",
